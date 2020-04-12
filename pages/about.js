33a017c5af537ae5b8be7d343df9150795929d8f
olena_rolskaya <manitskayaelena@gmail.com>
2020-04-12 19:05:50 +0300

    first commit

---
+++ pages/about.js
@@ -0,0 +1,13 @@
+import Layout from '../components/Layout'
+
+const About = () => (
+  <Layout>
+    <div>
+    <h1>About Crypto</h1>
+    <p>Application to view Crypto</p>
+  </div>
+  </Layout>
+  
+);
+
+export default About;
